b3152b59580feaf74fff56df35b60d6b191f5bc7
jobinben <1021137079@qq.com>
2022-01-25 16:19:59 +0800

    用模板和context进行渲染

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 const Vue = require('vue')
 const express = require('express')
+const vueServerRender = require('vue-server-renderer')
+const fs = require('fs')
+
+// 1. 创建本地服务器
 const server = express()
-const renderer = require('vue-server-renderer').createRenderer()
+
+// 2. 读取VUE SSR的模板
+const template = fs.readFileSync('./index.template.html', 'utf-8')
+
+// 3. 把模板引入到renderer
+const renderer = vueServerRender.createRenderer({
+    template
+})
+
+// 4. 定义一个上下文
+const context = {
+    title: 'Vue-SSR',
+    metas: `
+        <meta name="keyword" content="vue,ssr">
+        <meta name="description" content="vue-ssr-demo">
+    `
+}
 
 server.get('*', (req, res) => {
     const app = new Vue({
         data: {
             url: req.url
         },
+        // 这里的内容都会插入到<!--vue-ssr-outlet-->注释的下面
         template: `<div>Hi Vue SSR! current url: {{url}}</div>`
     })
-
-    renderer.renderToString(app, (err, html) => {
+    // 传入vue实例，上下文context
+    renderer.renderToString(app, context, (err, html) => {
         if (err) {
             res.status(500).end('服务器错误: 500')
             return
         }
-        res.end(
-            `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <title>vue ssr</title>
-            </head>
-            <body>
-             ${html}
-            </body>
-            </html>`
-        )
+        res.end(html)
     })
 })
 
