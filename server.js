f3df2d3a1d74318bf647d9b76cefc00d0e9c03cf
jobinben <1021137079@qq.com>
2022-01-25 15:30:12 +0800

    借助express渲染html在页面上

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const Vue = require('vue')
-const app = new Vue({
-    template: `<div>Hi Vue SSR!</div>`
-})
-
+const express = require('express')
+const server = express()
 const renderer = require('vue-server-renderer').createRenderer()
 
-renderer.renderToString(app, (err, html) => {
-    if(err) throw err
+server.get('*', (req, res) => {
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: `<div>Hi Vue SSR! current url: {{url}}</div>`
+    })
 
-    console.log(html)
+    renderer.renderToString(app, (err, html) => {
+        if (err) {
+            res.status(500).end('服务器错误: 500')
+            return
+        }
+        res.end(
+            `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>vue ssr</title>
+            </head>
+            <body>
+             ${html}
+            </body>
+            </html>`
+        )
+    })
 })
+
+server.listen(8080, () =>{
+    console.log('服务器已开启: 127.0.0.1:8080')
+})
+
